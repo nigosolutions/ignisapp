54308ba9c922b14ff8541526fa6cc589a14eaa56
nigosolutions <nigosolutions@gmail.com>
2022-04-10 22:31:48 +0300

    main

--- app/Screens/DashboardScreen.js
+++ app/Screens/DashboardScreen.js
@@ -9,12 +9,10 @@
     Stack, 
     NativeBaseProvider, 
     VStack,
-    Icon,
     Button
   } from "native-base";
 
-
-  import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Icon } from "@rneui/themed";
 
 function DashboardScreen(props) {
     return (
@@ -26,25 +24,31 @@ function DashboardScreen(props) {
              <HStack height={160} justifyContent={'center'} space={3}>
                 <Box rounded={15} bgColor={'#fff'} flex={1} >
                     <HStack mx={5} my={7} space={10} alignItems={'center'} >
-                        <Icon 
-                        size="10"
-                        as={<MaterialCommunityIcons name="pending" />} />
+                       <Icon
+                        reverse
+                        name='ios-american-football'
+                        type='ionicon'
+                        color='#517fa4'/>
                         <Text>Hello</Text>
                     </HStack>
                 </Box>
                 <Box  alignItems={'center'} justifyContent={'center'} rounded={15} bgColor={'#fff'} flex={1} >
                     <HStack alignItems={'center'} space={10} >
-                        <Icon 
-                        size="10"
-                        as={<MaterialCommunityIcons name="file" />} />
+                      <Icon
+                        reverse
+                        name='ios-american-football'
+                        type='ionicon'
+                        color='#517fa4'/>
                         <Text>Hello</Text>
                     </HStack>
                 </Box>
                 <Box rounded={15} bgColor={'#fff'} flex={1}>
                     <HStack mx={5} my={7} space={10} alignItems={'center'} >
-                        <Icon 
-                        size="10"
-                        as={<MaterialCommunityIcons name="file" />} />
+                    <Icon
+                        reverse
+                        name='ios-american-football'
+                        type='ionicon'
+                        color='#517fa4'/>
                         <Text>Hello</Text>
                     </HStack>
                 </Box>
